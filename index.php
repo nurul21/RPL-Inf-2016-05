806f11db5098a07134c736d8922ece34dd9f968b
pobi <pobi.rizki@s1.informatika.unsyiah.ac.id>
2016-06-02 22:04:31 +0700

    index.php with boostrap

---
+++ index.php
@@ -0,0 +1,115 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>E-Service</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Green Wheels Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<body>
+<script async type='text/javascript' src='//cdn.fancybar.net/ac/fancybar.js?zoneid=1502&serve=C6ADVKE&placement=w3layouts' id='_fancybar_js'></script>
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+
+<!-- top-header -->
+<div class="top-header">
+	<div class="container">
+		<ul class="tp-hd-lft" >
+			<li class="hm"><a href="index.html"><i class="fa fa-home"></i></a></li>
+			<li class="prnt" style="color:white"><a href="#"></a>Home</li>
+				
+		</ul>
+		<ul class="tp-hd-rgt"> 
+			<li class="tol">Call Service : 666-66666</li>				
+			<li class="sig"><a href="#"  >Loket Admin</a></li> 
+			<li class="sigi"><a href="#"  >/ Supir</a></li>
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /top-header ---->
+<!--- header ---->
+<div class="header">
+	<div class="container">
+		<div class="logo" >
+			<a href="index.php">E -<span> Service</span></a>	
+		</div>
+		<div class="bus">
+            <a href="index.html" class="buses active" >Mobil L300</a>
+        </div>
+		<div class="lock"> 
+			<li><i class="fa fa-lock"></i></li>
+            <li><div class="securetxt">AMAN &amp; TERPERCAYA<br> PEMBAYARAN ONLINE</div></li>
+			<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /header ---->
+<!--- footer-btm ---->
+<div class="footer-btm">
+	<div class="container">
+	<div class="navigation">
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+					<nav class="cl-effect-1">
+						<ul class="nav navbar-nav">
+							<li><a href="about.html">About</a></li>
+								<li><a href="faq.html">Faq</a></li>
+								<li><a href="apps.html">Apps</a></li>
+								<li><a href="blog.html">Blog</a></li>
+								<li><a href="travels.html">Travels</a></li>
+								<li><a href="privacy.html">Privacy Policy</a></li>
+								<li><a href="agent.html">Agent Registration</a></li>
+								<li><a href="terms.html">Terms of Use</a></li>
+								<li><a href="contact.html">Contact Us</a></li>
+								<li>Need Help?<a href="#" data-toggle="modal" data-target="#myModal3"> / Write Us </a>  </li>
+								<div class="clearfix"></div>
+						</ul>
+					</nav>
+				</div><!-- /.navbar-collapse -->	
+			</nav>
+		</div>
+		
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /footer-btm ---->
+<!--- banner ---->
+<div class="banner">
+	<div class="container">
+		<h1 class="" > <a href="index.php">E -<span style="color:#34ad00"> Service</span></a> - Merupakan layanan penjualan tiket online mobil L300</h1>
+	</div>
+</div>
+<!---728x90--->
+</body>
+</html>
