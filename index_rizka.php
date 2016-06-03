d93db99cd2f8f7541b36574d5cba49cff7c00a88
Rizka Ramadhana <rizkaramadhanaa@gmail.com>
2016-06-03 11:40:25 +0700

    no message

---
+++ index_rizka.php
@@ -0,0 +1,113 @@
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>E-Service</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Green Wheels Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<body>
+<script async type='text/javascript' src='//cdn.fancybar.net/ac/fancybar.js?zoneid=1502&serve=C6ADVKE&placement=w3layouts' id='_fancybar_js'></script>
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+
+
+				<link rel="stylesheet" href="css/jquery-ui.css" />
+				<script src="js/jquery-ui.js"></script>
+
+		</div>
+		<div class="sear">
+			<form action="bus.html">
+				<button class="seabtn">Search Car</button>
+			</form>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="container">
+	<div class="rupes">
+		<div class="col-md-4 rupes-left">
+			<div class="rup-left">
+				<a href="offers.html"><i class="fa fa-usd"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>UP TO IDR 10.000 OFF</h3>
+				<h4><a href="offers.html">TRAVEL SMART</a></h4>
+				<p>CODE:YBMAR12<br>Book Using Pay Money</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 rupes-left ">
+			<div class="rup-left">
+				<a href="offers.html"><i class="fa fa-bus"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>UP TO 70% OFF</h3>
+				<h4><a href="offers.html">ON HOTELS ACROSS WORLD</a></h4>
+				<p>Offer CODE:YBMAR12</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 rupes-left ">
+			<div class="rup-left">
+				<a href="offers.html"><i class="fa fa-mobile"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>FLAT IDR 10.000 OFF</h3>
+				<h4><a href="offers.html">BUS APP OFFER</a></h4>
+				<p>book via the yellow Bus App<br>CODE:YBMAR12</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="holiday">
+		<div class="col-md-3 holiday-left animated wow fadeInUp animated" data-wow-duration="1200ms" data-wow-delay="500ms" style="visibility: visible; animation-duration: 1200ms; animation-delay: 500ms; animation-name: fadeInUp;">
+			<img src="images/4.jpg" class="img-responsive" alt="">
+		</div>
+		<div class="col-md-6 holiday-mid animated wow fadeInUp animated" >
+		<h3>Packet Travel</h3>
+		<p>Nikmati Perjalanan Menggunakan CV terbaik kami. Untuk informasi lebih lanjut hubungi kami....</p>
+		</div>
+		<div class="col-md-3 holiday-left animated ">
+			<img src="images/5.jpg" class="img-responsive" alt="">
+		</div>
+			<div class="clearfix"></div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="map">
+                <iframe src="https://www.google.com/maps/embed/v1/place?q=Terminal+Bus+Batoh,+Batoh,+Banda+Aceh+City,+Aceh,+Indonesia&key=AIzaSyAN0om9mFmy1QN6Wf54tXAowK4eT0ZUPrU" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div> 
+	<div class="track">	
+			<h3>Lokasi Kami</h3>
+			<center>
+			<p>JL. Dr.Ir T.Moh.Hassan</p>
+			</center>
+			<a href="track.html" class="learn">Visit Me</a>
+		</div>
+			<div class="clearfix"></div>
+	</div>
+</div>
+
+</body>
+</html>
